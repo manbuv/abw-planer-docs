0721a38be4374adaa0befd6542d8adb81257ac3d
manbuv <60511289+manbuv@users.noreply.github.com>
2023-03-17 09:09:52 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+html_theme_options = {
+    'logo_only': False,
+    'html_logo': '_static/images/logo.svg',
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
